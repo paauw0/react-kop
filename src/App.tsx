d558dc0342251bab0f8bc2c1e3b1742e7b460b78
paauw0 <970610851@qq.com>
2021-04-13 21:49:42 +0800

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Button, { ButtonType, ButtonSize } from './components/Button/button'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <Button className="custom" autoFocus>Hello</Button>
+        <Button onClick={(e) => {e.preventDefault(); alert(123);}}>Hello</Button>
+        <Button disabled>Disabled Button</Button>
+        <Button btnType={ButtonType.Primary} size={ButtonSize.Large}>Large Primary</Button>
+        <Button btnType={ButtonType.Danger} size={ButtonSize.Small}>Small Danger</Button>
+        <Button btnType={ButtonType.Link} href="http://www.baidu.com" target="_blank">Baidu Link</Button>
+        <Button disabled btnType={ButtonType.Link} href="http://www.baidu.com">Disabled Link</Button>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
